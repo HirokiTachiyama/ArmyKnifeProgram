b23268f81a142a2665622f643d08524c9fcb6eb6
student <student@localhost.localdomain>
2018-07-02 12:14:55 +0900

    add dice rolo test

---
+++ src/workshop/dice_role.rb
@@ -0,0 +1,52 @@
+require 'curses'
+require 'color_echo/get'
+
+mwin = Curses.init_screen
+mwin.box(?|, ?-, ?+)
+
+begin
+	r = Random.new
+	
+	mwin_str  = "Main Window"
+	swin1_str = "Sub Window 1"
+	swin2_str = "Sub Window 2"
+
+	Curses.setpos(Curses.lines / 2, Curses.cols / 2 - (mwin_str.length / 2))
+	# Curses.addstr(mwin_str)
+
+	swin1 = mwin.subwin(3, Curses.cols, 0, 0)
+	swin1.box(?|, ?-, ?+)
+	swin1.setpos(1, Curses.cols / 2 - (swin1_str.length / 2))
+	swin1.addstr(swin1_str)
+	swin1.refresh
+
+	swin2 = mwin.subwin(3, Curses.cols, Curses.lines - 3, 0)
+	swin2.box(?|, ?-, ?+)
+	swin2.setpos(1, Curses.cols / 2 - (swin2_str.length / 2))
+	# swin2.addstr(swin2_str)
+	swin2.refresh
+
+	# Curses.refresh
+	# Curses.getch
+	loop do 
+		mwin_str = t CE.once.fg(:yellow).get(str)
+		mwin_str = CE.fg(:yellow).r.rand(1..6).to_s+" "
++r.rand(1..6).to_s+" "+r.rand(1..6).to_s
+		Curses.setpos(Curses.lines / 2, Curses.cols / 2 - (mwin_str.length / 2))
+		# Curses.delch
+		Curses.addstr(mwin_str)
+	
+		swin2.setpos(1, Curses.cols / 2 - (swin2_str.length / 2))
+		swin2_str = r.rand(1..9).to_s
+		swin2.addstr(swin2_str)
+		 swin2.refresh
+
+Curses.refresh
+		sleep(0.1)
+
+	end
+
+ensure
+	Curses.close_screen
+end
+	
