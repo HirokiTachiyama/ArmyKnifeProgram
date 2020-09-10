0bb97fa751e70508c32a0884840979f64befc928
HirokiTachiyama <hirokitachiyama@gmail.com>
2020-09-11 00:08:46 +0900

    work on arch

--- src/shogi-kifu-mode/KifuReader.rb
+++ src/shogi-kifu-mode/KifuReader.rb
@@ -1,27 +1,52 @@
 
+$goban = nil
+$kifu  = nil
+
+def show_goban
+  puts "SENTE:" + $sente
+  puts $goban
+  puts "GOTE:"  + $gote
+end
+
+def prepare_goban
+  $goban = Hash.new { |h, k| h[k] = {} }
+  $goban[""]
+
+end
+
+def show_kifu
+  puts $kifu
+end
+
 File.open("kifu.csa") do |f|
   f.gets # Version
-  $sente = f.gets #
-  $gote  = f.gets
-  $event = f.gets
-  $site  = f.gets
-  $start_time = f.gets
-  $opening = f.gets
+  $sente = f.gets.delete("N+")
+  $gote  = f.gets.delete("N-")
+  $event = f.gets.delete("$EVENT:")
+  $site  = f.gets.delete("$SITE:")
+  $start_time = f.gets.delete("$START_TIME:")
+  $opening = f.gets.delete("$OPENING:")
 
+  while( !f.gets.include?("+\n") )  # throw away P1~P9
+  end
+  
   $kifu = ""
   f.each_line do |l|
     $kifu = $kifu + l
   end
 end
 
-puts $sente
-puts $gote
-puts $event
-puts $site
-puts $start_time
-puts $opening
+puts "-----"
 
 $kifu.each_line do |l|
   print l
 end
+
+loop do
+  cmd = gets.chomp
+  show_goban if cmd.eql? "g"
+  show_kifu  if cmd.eql? "k"
+end
+
+
 
