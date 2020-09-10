cc99347509b720d757c21e7645ed42cb90a306a7
tachiyama <hirokitachiyama@gmail.com>
2020-09-11 00:36:22 +0900

    make goban

--- src/shogi-kifu-mode/KifuReader.rb
+++ src/shogi-kifu-mode/KifuReader.rb
@@ -1,3 +1,4 @@
+# coding: cp932
 
 $goban = nil
 $kifu  = nil
@@ -10,8 +11,47 @@ end
 
 def prepare_goban
   $goban = Hash.new { |h, k| h[k] = {} }
-  $goban[""]
+  $goban["ˆê"] = {1=>["-", "KY"],
+                  3=>["-", "FU"],
+                  7=>["+", "FU"],
+                  9=>["+", "KY"]}
 
+  $goban["“ñ"] = {1=>["-", "KE"],
+                  2=>["-", "KA"],
+                  3=>["-", "FU"],
+                  7=>["+", "FU"],
+                  8=>["+", "HI"],
+                  9=>["+", "KE"]}
+
+  $goban["ŽO"] = {1=>["-", "KY"],
+                  3=>["-", "FU"],
+                  7=>["+", "FU"],
+                  9=>["+", "KY"]}
+
+
+  $goban["Žl"] = {1=>["-", "KY"],
+                  3=>["-", "FU"],
+                  7=>["+", "FU"],
+                  9=>["+", "KY"]}
+
+  $goban["ŒÜ"] = {1=>["-", "KY"],
+                  3=>["-", "FU"],
+                  7=>["+", "FU"],
+                  9=>["+", "KY"]}
+
+  $goban["˜Z"] = {1=>["-", "KY"],
+                  3=>["-", "FU"],
+                  7=>["+", "FU"],
+                  9=>["+", "KY"]}
+
+  $goban["Žµ"] = {1=>["-", "KY"],
+                  3=>["-", "FU"],
+                  7=>["+", "FU"],
+                  9=>["+", "KY"]}
+  $goban["”ª"] = {}
+  
+  
+  $goban["‹ã"][9] = [ "+", "KY" ]
 end
 
 def show_kifu
@@ -28,12 +68,14 @@ File.open("kifu.csa") do |f|
   $opening = f.gets.delete("$OPENING:")
 
   while( !f.gets.include?("+\n") )  # throw away P1~P9
+    print "j"
   end
-  
+
   $kifu = ""
   f.each_line do |l|
     $kifu = $kifu + l
   end
+
 end
 
 puts "-----"
@@ -42,11 +84,15 @@ $kifu.each_line do |l|
   print l
 end
 
+prepare_goban
+
 loop do
   cmd = gets.chomp
   show_goban if cmd.eql? "g"
   show_kifu  if cmd.eql? "k"
 end
+
+puts $goban["‹ã"][9]
 
 
 
